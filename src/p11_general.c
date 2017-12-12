2438a8f3dfb1418e1a7134b33dfde8bc71483e3f
Sumit Garg <sumit.garg@nxp.com>
2017-12-13 04:42:53 +0530

    Make global function list static & do runtime init
    Signed-off-by: Sumit Garg <sumit.garg@nxp.com>

--- src/p11_general.c
+++ src/p11_general.c
@@ -25,81 +25,7 @@ static CK_INFO cryptoki_info;
 /*
  * List of function entry points
  */
-CK_FUNCTION_LIST global_function_list = {
-	.version =				{0x2, 0x28},
-	.C_Initialize =				C_Initialize,
-	.C_Finalize  =				C_Finalize,
-	.C_GetInfo  =				C_GetInfo,
-	.C_GetFunctionList  =			C_GetFunctionList,
-	.C_GetSlotList  =			C_GetSlotList,
-	.C_GetSlotInfo  =			C_GetSlotInfo,
-	.C_GetTokenInfo  =			C_GetTokenInfo,
-	.C_GetMechanismList  =			C_GetMechanismList,
-	.C_GetMechanismInfo  =			C_GetMechanismInfo,
-	.C_InitToken  =				C_InitToken,
-	.C_InitPIN  =				C_InitPIN,
-	.C_SetPIN  =				C_SetPIN,
-	.C_OpenSession  =			C_OpenSession,
-	.C_CloseSession  =			C_CloseSession,
-	.C_CloseAllSessions  =			C_CloseAllSessions,
-	.C_GetSessionInfo  =			C_GetSessionInfo,
-	.C_GetOperationState  =			C_GetOperationState,
-	.C_SetOperationState  =			C_SetOperationState,
-	.C_Login  =				C_Login,
-	.C_Logout  =				C_Logout,
-	.C_CreateObject  =			C_CreateObject,
-	.C_CopyObject  =			C_CopyObject,
-	.C_DestroyObject  =			C_DestroyObject,
-	.C_GetObjectSize  =			C_GetObjectSize,
-	.C_GetAttributeValue  =			C_GetAttributeValue,
-	.C_SetAttributeValue  =			C_SetAttributeValue,
-	.C_FindObjectsInit  =			C_FindObjectsInit,
-	.C_FindObjects  =			C_FindObjects,
-	.C_FindObjectsFinal  =			C_FindObjectsFinal,
-#if 0
-	.C_EncryptInit  =			C_EncryptInit,
-	.C_Encrypt  =				C_Encrypt,
-	.C_EncryptUpdate  =			C_EncryptUpdate,
-	.C_EncryptFinal  =			C_EncryptFinal,
-	.C_DecryptInit  =			C_DecryptInit,
-	.C_Decrypt  =				C_Decrypt,
-	.C_DecryptUpdate  =			C_DecryptUpdate,
-	.C_DecryptFinal  =			C_DecryptFinal,
-	.C_DigestInit  =			C_DigestInit,
-	.C_Digest  =				C_Digest,
-	.C_DigestUpdate  =			C_DigestUpdate,
-	.C_DigestKey  =				C_DigestKey,
-	.C_DigestFinal  =			C_DigestFinal,
-	.C_SignInit  =				C_SignInit,
-	.C_Sign  =				C_Sign,
-	.C_SignUpdate  =			C_SignUpdate,
-	.C_SignFinal  =				C_SignFinal,
-	.C_SignRecoverInit  =			C_SignRecoverInit,
-	.C_SignRecover  =			C_SignRecover,
-	.C_VerifyInit  =			C_VerifyInit,
-	.C_Verify  =				C_Verify,
-	.C_VerifyUpdate  =			C_VerifyUpdate,
-	.C_VerifyFinal  =			C_VerifyFinal,
-	.C_VerifyRecoverInit  =			C_VerifyRecoverInit,
-	.C_VerifyRecover  =			C_VerifyRecover,
-	.C_DigestEncryptUpdate  =		C_DigestEncryptUpdate,
-	.C_DecryptDigestUpdate  =		C_DecryptDigestUpdate,
-	.C_SignEncryptUpdate  =			C_SignEncryptUpdate,
-	.C_DecryptVerifyUpdate  =		C_DecryptVerifyUpdate,
-	.C_GenerateKey  =			C_GenerateKey,
-	.C_GenerateKeyPair  =			C_GenerateKeyPair,
-	.C_WrapKey  =				C_WrapKey,
-	.C_UnwrapKey  =				C_UnwrapKey,
-	.C_DeriveKey  =				C_DeriveKey,
-	.C_SeedRandom  =			C_SeedRandom,
-	.C_GenerateRandom  =			C_GenerateRandom,
-#endif
-	.C_GetFunctionStatus  =			C_GetFunctionStatus,
-	.C_CancelFunction  =			C_CancelFunction,
-#if 0
-	.C_WaitForSlotEvent  =			C_WaitForSlotEvent,
-#endif
-};
+static CK_FUNCTION_LIST global_function_list;
 
 /*
  *  GENERAL-PURPOSE FUNCTIONS
@@ -214,6 +140,80 @@ CK_RV C_GetFunctionList(CK_FUNCTION_LIST_PTR_PTR ppFunctionList)
 	if (ppFunctionList == NULL)
 		return CKR_ARGUMENTS_BAD;
 
+	global_function_list.version.major =		0x2;
+	global_function_list.version.minor =		0x28;
+	global_function_list.C_Initialize =		C_Initialize;
+	global_function_list.C_Finalize  =		C_Finalize;
+	global_function_list.C_GetInfo  =		C_GetInfo;
+	global_function_list.C_GetFunctionList =	C_GetFunctionList;
+	global_function_list.C_GetSlotList  =		C_GetSlotList;
+	global_function_list.C_GetSlotInfo  =		C_GetSlotInfo;
+	global_function_list.C_GetTokenInfo  =		C_GetTokenInfo;
+	global_function_list.C_GetMechanismList  =	C_GetMechanismList;
+	global_function_list.C_GetMechanismInfo  =	C_GetMechanismInfo;
+	global_function_list.C_InitToken  =		C_InitToken;
+	global_function_list.C_InitPIN  =		C_InitPIN;
+	global_function_list.C_SetPIN  =		C_SetPIN;
+	global_function_list.C_OpenSession  =		C_OpenSession;
+	global_function_list.C_CloseSession  =		C_CloseSession;
+	global_function_list.C_CloseAllSessions  =	C_CloseAllSessions;
+	global_function_list.C_GetSessionInfo  =	C_GetSessionInfo;
+	global_function_list.C_GetOperationState  =	C_GetOperationState;
+	global_function_list.C_SetOperationState  =	C_SetOperationState;
+	global_function_list.C_Login  =			C_Login;
+	global_function_list.C_Logout  =		C_Logout;
+	global_function_list.C_CreateObject  =		C_CreateObject;
+	global_function_list.C_CopyObject  =		C_CopyObject;
+	global_function_list.C_DestroyObject  =		C_DestroyObject;
+	global_function_list.C_GetObjectSize  =		C_GetObjectSize;
+	global_function_list.C_GetAttributeValue  =	C_GetAttributeValue;
+	global_function_list.C_SetAttributeValue  =	C_SetAttributeValue;
+	global_function_list.C_FindObjectsInit  =	C_FindObjectsInit;
+	global_function_list.C_FindObjects  =		C_FindObjects;
+	global_function_list.C_FindObjectsFinal  =	C_FindObjectsFinal;
+#if 0
+	global_function_list.C_EncryptInit  =		C_EncryptInit;
+	global_function_list.C_Encrypt  =		C_Encrypt;
+	global_function_list.C_EncryptUpdate  =		C_EncryptUpdate;
+	global_function_list.C_EncryptFinal  =		C_EncryptFinal;
+	global_function_list.C_DecryptInit  =		C_DecryptInit;
+	global_function_list.C_Decrypt  =		C_Decrypt;
+	global_function_list.C_DecryptUpdate  =		C_DecryptUpdate;
+	global_function_list.C_DecryptFinal  =		C_DecryptFinal;
+	global_function_list.C_DigestInit  =		C_DigestInit;
+	global_function_list.C_Digest  =		C_Digest;
+	global_function_list.C_DigestUpdate  =		C_DigestUpdate;
+	global_function_list.C_DigestKey  =		C_DigestKey;
+	global_function_list.C_DigestFinal  =		C_DigestFinal;
+	global_function_list.C_SignInit  =		C_SignInit;
+	global_function_list.C_Sign  =			C_Sign;
+	global_function_list.C_SignUpdate  =		C_SignUpdate;
+	global_function_list.C_SignFinal  =		C_SignFinal;
+	global_function_list.C_SignRecoverInit  =	C_SignRecoverInit;
+	global_function_list.C_SignRecover  =		C_SignRecover;
+	global_function_list.C_VerifyInit  =		C_VerifyInit;
+	global_function_list.C_Verify  =		C_Verify;
+	global_function_list.C_VerifyUpdate  =		C_VerifyUpdate;
+	global_function_list.C_VerifyFinal  =		C_VerifyFinal;
+	global_function_list.C_VerifyRecoverInit  =	C_VerifyRecoverInit;
+	global_function_list.C_VerifyRecover  =		C_VerifyRecover;
+	global_function_list.C_DigestEncryptUpdate  =	C_DigestEncryptUpdate;
+	global_function_list.C_DecryptDigestUpdate  =	C_DecryptDigestUpdate;
+	global_function_list.C_SignEncryptUpdate  =	C_SignEncryptUpdate;
+	global_function_list.C_DecryptVerifyUpdate  =	C_DecryptVerifyUpdate;
+	global_function_list.C_GenerateKey  =		C_GenerateKey;
+	global_function_list.C_GenerateKeyPair  =	C_GenerateKeyPair;
+	global_function_list.C_WrapKey  =		C_WrapKey;
+	global_function_list.C_UnwrapKey  =		C_UnwrapKey;
+	global_function_list.C_DeriveKey  =		C_DeriveKey;
+	global_function_list.C_SeedRandom  =		C_SeedRandom;
+	global_function_list.C_GenerateRandom  =	C_GenerateRandom;
+#endif
+	global_function_list.C_GetFunctionStatus  =	C_GetFunctionStatus;
+	global_function_list.C_CancelFunction  =	C_CancelFunction;
+#if 0
+	global_function_list.C_WaitForSlotEvent  =	C_WaitForSlotEvent;
+#endif
 	*ppFunctionList = &global_function_list;
  
 	return CKR_OK;
